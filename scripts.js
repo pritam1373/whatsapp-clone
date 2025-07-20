e38b20aca6b822c4e096a09043b832208182cba5
pritam1373 <ovnandy99@gmail.com>
2025-07-20 14:40:52 +0530

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,60 @@
+p=document.getElementById("pritam")
+s=document.getElementById("sumanta")
+m=document.getElementById("ma")
+b=document.getElementById("baba")
+f=document.getElementById("flipkard")
+l=document.getElementById("laltu")
+g=document.getElementById("ghosh")
+s=document.getElementById("shirshak")
+d=document.getElementById("debkumar")
+
+
+o=document.getElementById("ovi")
+on=document.getElementById("ovinandy")
+
+on.addEventListener("click",  () =>{
+  o.style.opacity=0
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+p.addEventListener("click", ()=>{
+    window.location.href="index1.html"
+})
+
+s.addEventListener("click", ()=>{
+    window.location.href="index2.html"
+})
+m.addEventListener("click", ()=>{
+    window.location.href="index3.html"
+})
+b.addEventListener("click", ()=>{
+    window.location.href="index4.html"
+})
+f.addEventListener("click", ()=>{
+    window.location.href="index5.html"
+})
+l.addEventListener("click", ()=>{
+    window.location.href="index6.html"
+})
+g.addEventListener("click", ()=>{
+    window.location.href="index7.html"
+})
+s.addEventListener("click", ()=>{
+    window.location.href="index8.html"
+})
+d.addEventListener("click", ()=>{
+    window.location.href="index9.html"
+})
+
